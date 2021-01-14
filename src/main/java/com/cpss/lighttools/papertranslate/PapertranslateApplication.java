9a8de8b5c597b652c9b6e62a3540305d4ca4924a
Weiguang Liu <1486111482@qq.com>
2021-01-14 10:55:43 +0800

    Initial commit

---
+++ src/main/java/com/cpss/lighttools/papertranslate/PapertranslateApplication.java
@@ -0,0 +1,13 @@
+package com.cpss.lighttools.papertranslate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapertranslateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PapertranslateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cpss/lighttools/papertranslate/PapertranslateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpss.lighttools.papertranslate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapertranslateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
